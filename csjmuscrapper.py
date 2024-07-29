1cdbaa03cc0c2580696580368e21919a08690efa
Sanjay Singh <sanjaysinghg2u@gmail.com>
2024-07-29 21:54:35 +0530

    optimized code and remove time

--- csjmuscrapper.py
+++ csjmuscrapper.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import UnexpectedAlertPresentException
 
 import edgedriver_autoinstaller
-import time, os
+import os
 import base64
 
 edgedriver_autoinstaller.install()
@@ -29,16 +29,7 @@ baseDetails = {
     "sem": "4"
 }
 
-Students = [
-    {
-        "rollno": "22015003570",
-        "dob": "07/27/2005" # Month/Day/Year
-    },
-    {
-        "rollno": "22015003600",
-        "dob": "11/28/2000" # Month/Day/Year
-    }
-]
+Students = [{'name': 'NILESH KUMAR ', 'rollno': 22015003581, 'dob': '03/10/2006'}, {'name': 'SHALINI SINGH', 'rollno': 22015003600, 'dob': '11/28/2000'}, {'name': 'KARTIKAY GAUTAM', 'rollno': 22015003570, 'dob': '07/27/2005'}]
 
 
 def save_as_pdf(driver, path):
@@ -63,71 +54,90 @@ def save_as_pdf(driver, path):
     with open(path, 'wb') as file:
         file.write(pdf_data)
 
-try:
-    for st in Students:
-        try:
-            # Open Google Shopping
-            driver.get("https://admission.csjmu.ac.in/DisplayResult/Index")
+def run():
+    try:
+        for st in Students:
+            try:
+                # Open CSJMU Website
+                driver.get("https://admission.csjmu.ac.in/DisplayResult/Index")
+
+                # Fill session id
+                dropdown_sessionid = driver.find_element(By.ID, "SessionID")
+                select = Select(dropdown_sessionid)
+                select.select_by_value(baseDetails['sessionId'])
+
+                # Wait for api to populate exam category
+                WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, f"//option[@value='{baseDetails['category']}']"))
+                )
+
+                dropdown_examcode = driver.find_element(By.ID, "ExamCategoryCode")
+                select = Select(dropdown_examcode)
+                select.select_by_value(baseDetails['category'])
 
 
-            dropdown_sessionid = driver.find_element(By.ID, "SessionID")
-            select = Select(dropdown_sessionid)
-            select.select_by_value(baseDetails['sessionId'])
+                # wait for api to populate course options
+                WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, f"//option[text()='{baseDetails['course']}']"))
+                )
+                
+                # click on course option
+                dropdown_course = driver.find_element(By.XPATH, "//span[@id='select2-SubCourseID-container']")
+                dropdown_course.click()
 
-            time.sleep(1)
+                # wait and click on course 
+                dropdown_option = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, f"//li[@class='select2-results__option' and text()='{baseDetails['course']}']")))
+                dropdown_option.click()
+                
+                
+                # wait for api to populate semester option
+                WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, f"//option[@value='{baseDetails['sem']}']"))
+                )
 
-            dropdown_examcode = driver.find_element(By.ID, "ExamCategoryCode")
-            select = Select(dropdown_examcode)
-            select.select_by_value(baseDetails['category'])
-
-            time.sleep(1)
-        
-            dropdown = driver.find_element(By.XPATH, "//span[@id='select2-SubCourseID-container']")
-            dropdown.click()
-
-            wait = WebDriverWait(driver, 10)
-            option = wait.until(EC.visibility_of_element_located((By.XPATH, f"//li[@class='select2-results__option' and text()='{baseDetails['course']}']")))
-            option.click()
-            
-            time.sleep(1)
-
-            dropdown_examcode = driver.find_element(By.ID, "SemYearID")
-            select = Select(dropdown_examcode)
-            select.select_by_value(baseDetails['sem'])
+                dropdown_sem = driver.find_element(By.ID, "SemYearID")
+                select = Select(dropdown_sem)
+                select.select_by_value(baseDetails['sem'])
 
 
-            time.sleep(1)
+                # Fill rollno
+                rollno_input = driver.find_element(By.ID, "StRollNo")
+                rollno_input.send_keys(st['rollno'])
 
-            date_input = driver.find_element(By.ID, "StRollNo")
-            date_input.send_keys(st['rollno'])
+                # Fill date 
+                date_input = driver.find_element(By.ID, "StDOB")
+                date_input.send_keys(st['dob'])
 
-            time.sleep(1)
+                
+                # Wait for button to appear and click
+                view_button = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.CLASS_NAME, "btn-primary"))
+                )
+                view_button.click()
 
-            date_input = driver.find_element(By.ID, "StDOB")
-            date_input.send_keys(st['dob'])
+                # Wait for print button and save the result
+                print_button = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, "//input[@type='button' and @value='Print Result']"))
+                )
 
-            time.sleep(1)
+                if print_button:
+                    # print(True)
+                    save_as_pdf(driver, f"Results/result-{st['rollno']}.pdf")
+                    print(f"---- Download Result Pdf!! {st['rollno']} ----")
+                
 
-            view_button = driver.find_element(By.CLASS_NAME, "btn-primary")
-            view_button.click()
+            except UnexpectedAlertPresentException as e:
+                print(e)
 
-            print_button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//input[@type='button' and @value='Print Result']"))
-        )
-
-            if print_button:
-                print(True)
-                save_as_pdf(driver, f"Results/result-{st['rollno']}.pdf")
-                print(f"---- Download Result Pdf!! {st['rollno']} ----")
-            
-
-        except UnexpectedAlertPresentException as e:
-            print(e)
-
-        except Exception as e:
-            print(f"An error occurred: {e}")
+            except Exception as e:
+                print(f"An error occurred: {e}")
 
 
-finally:
-    # Close the browser
-    driver.quit()
+    finally:
+        # Close the browser
+        driver.quit()
+
+
+
+if __name__=="__main__":
+    run()
